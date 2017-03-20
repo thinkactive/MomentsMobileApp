c14eb838c44415ed0aca66013b7acfb9d8d44ecf
quaketech <jatin@quaketech.org>
2017-03-20 09:50:14 -0400

    Add project files.

---
+++ MomentsMobileAppService/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MomentsMobileAppService.Startup))]
+
+namespace MomentsMobileAppService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
